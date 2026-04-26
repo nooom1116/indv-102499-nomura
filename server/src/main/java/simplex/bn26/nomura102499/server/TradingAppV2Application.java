d01c7cbdc7a6b639d03f15c01ed5f150f2b680ba
ryotaroNomu <ryo34no@gmail.com>
2026-04-26 11:39:25 +0900

    Initial commit: Setup React and Spring project

---
+++ server/src/main/java/simplex/bn26/nomura102499/server/TradingAppV2Application.java
@@ -0,0 +1,13 @@
+package simplex.bn26.nomura102499.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAppV2Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/simplex/bn26/nomura102499/server/TradingAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package simplex.bn26.nomura102499.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
